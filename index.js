10695b0aa7cfa467562f1b87f2730d098e62d676
minazul01 <minajul4010@gmail.com>
2025-06-26 10:05:22 +0600

    comment section added

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const express = require('express');
-require('dotenv').config()
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const express = require("express");
+require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
-const cors = require('cors');
-
+const cors = require("cors");
 
 // midleware
 app.use(cors());
 app.use(express.json());
-
-
 
 // mongodb server
 
@@ -22,7 +19,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -30,31 +27,45 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
-
     const blogCollection = client.db("tech-blog").collection("post");
     const commentCollection = client.db("tech-blog").collection("comment");
     // some post insert direct mongodb
-    app.get('/post', async (req, res) => {
-        const result = await blogCollection.find().toArray();
-        res.send(result);
+    app.get("/post", async (req, res) => {
+      const result = await blogCollection.find().toArray();
+      res.send(result);
+    });
+    app.get("/post/:id", async (req, res) => {
+      const id = req.params.id;
+      const result = await blogCollection.findOne({ _id: new ObjectId(id) });
+      res.send(result);
     });
 
-
-    // comment post 
-    app.post("/comoment", async (req, res) => {
+    // comment post
+    app.post("/comment", async (req, res) => {
       const query = req.body;
-      const result = await likeCollection.insertOne(query);
-      res.send(result);
-    });
-    app.get("/comment", async (req, res) => {
-      const result = await likeCollection.find().toArray();
+      const result = await commentCollection.insertOne(query);
       res.send(result);
     });
 
+    // All comments
+    app.get("/comment", async (req, res) => {
+      const result = await commentCollection.find().toArray();
+      res.send(result);
+    });
+
+    // Specific post's comments by postId
+    app.get("/comment/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { postId: id }; // Post create করার সময় অবশ্যই postId পাঠাবেন
+      const comments = await commentCollection.find(query).toArray();
+      res.send(comments);
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -62,13 +73,10 @@ async function run() {
 }
 run().catch(console.dir);
 
-
-
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
