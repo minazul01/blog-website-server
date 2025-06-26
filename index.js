d730562f037fb4a3f19455cefcffbb5ab042eab2
minazul01 <minajul4010@gmail.com>
2025-06-27 03:05:38 +0600

    all done

--- index.js
+++ index.js
@@ -2,12 +2,30 @@ const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const express = require("express");
 require("dotenv").config();
 const app = express();
+const jwt = require("jsonwebtoken");
+const cokieParser = require("cookie-parser");
 const port = process.env.PORT || 5000;
 const cors = require("cors");
 
 // midleware
 app.use(cors());
 app.use(express.json());
+app.use(cokieParser());
+
+// jwt token
+const verifyToken = (req, res, next) => {
+  const token = req?.cookies?.token;
+  if (!token) {
+    return res.status(401).send({ message: "unautorize access" });
+  }
+  jwt.verify(token, process.env.JWT_Token, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ message: "unautorize access" });
+    }
+    req.user = decoded;
+    next();
+  });
+};
 
 // mongodb server
 
@@ -25,10 +43,11 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     const blogCollection = client.db("tech-blog").collection("post");
     const commentCollection = client.db("tech-blog").collection("comment");
+    const favoriteCollection = client.db("tech-blog").collection("favorite");
     // some post insert direct mongodb
     app.get("/post", async (req, res) => {
       const result = await blogCollection.find().toArray();
@@ -49,29 +68,42 @@ async function run() {
     // remove the blog post post
     app.delete("/post/:id", async (req, res) => {
       const id = req.params.id;
-      const query = {_id: new ObjectId(id)};
+      const query = { _id: new ObjectId(id) };
       const result = await blogCollection.deleteOne(query);
       res.send(result);
     });
 
-    // Update the post 
+    // jwt token authentication
+    // authentication apis with token
+    app.post("/jwt", verifyToken, (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.JWT_Token, { expiresIn: "1d" });
+      res
+        .cookie("token", token, {
+          httpOnly: true,
+          secure: false, // set to true if using HTTPS
+        })
+        .send({ success: true });
+    });
+
+    // Update the post
     app.get("/post/:id", async (req, res) => {
       const id = req.params.id;
-      const query = {_id: new ObjectId(id)};
+      const query = { _id: new ObjectId(id) };
       const result = await blogCollection.findOne(query);
       res.send(result);
     });
     app.patch("/post/:id", async (req, res) => {
       const id = req.params.id;
       const data = req.body;
-      const query = {_id: new ObjectId(id)};
+      const query = { _id: new ObjectId(id) };
       const upadate = {
         $set: {
           title: data.title,
           category: data.category,
-          description: data.description
-        }
-      }
+          description: data.description,
+        },
+      };
       const result = await blogCollection.updateOne(query, upadate);
       res.send(result);
     });
@@ -97,11 +129,40 @@ async function run() {
       res.send(comments);
     });
 
+    // favorite blog post
+    app.post("/favorite", async (req, res) => {
+      const data = req.body;
+      if (!Array.isArray(data) || data.length === 0) {
+        return res.status(400).send({ message: "No documents to insert" });
+      }
+      // Insert many expects an array of documents
+      const result = await favoriteCollection.insertMany(data);
+      res.send(result);
+    });
+    app.get("/favorite/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await favoriteCollection.findOne(query);
+      res.send(result);
+    });
+    app.get("/favorite", async (req, res) => {
+      const result = await favoriteCollection.find().toArray();
+      res.send(result);
+    });
+    // favorite blog post delete
+
+    app.delete("/favorite/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await favoriteCollection.deleteOne(query);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
+    // await client.db("admin").command({ ping: 1 });
+    // console.log(
+    //   "Pinged your deployment. You successfully connected to MongoDB!"
+    // );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
